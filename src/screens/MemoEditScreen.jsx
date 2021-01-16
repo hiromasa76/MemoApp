12b2e8eedc94bc285c3e8623682e7f8e0c7b9cf6
hiromasa76 <63169902+hiromasa76@users.noreply.github.com>
2021-01-17 06:49:26 +0900

    Update memo data on MemoEditScreen

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,25 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-    View, TextInput, StyleSheet,
+    View, TextInput, StyleSheet, Alert,
 } from 'react-native';
+import { shape, string } from 'prop-types';
+import firebase from 'firebase'
 
 import CircleButton from '../components/CircleButton';
-import KeybordSafeview from '../components/KeybordSafeview'
+import KeybordSafeview from '../components/KeybordSafeview';
 
 
 export default function MemoEditScreen(props) {
-    const { navigation } = props;
+    const { navigation, route } = props;
+    const { id, bodyText } = route.params;
+    const [body, setBody] = useState(bodyText);
+
+    function handlePress() {
+        const { currentUser } = firebase.auth();
+        console.log(currentUser.uid)
+        if (currentUser) {
+            const db = firebase.firestore();
+            const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+            ref.set({
+                bodyText: body,
+                updatedAt: new Date(),
+            }, { merge: true })
+                .then(() => {
+                    navigation.goBack();
+                })
+                .catch((error) => {
+                    Alert.alert(error.code)
+                });
+        }
+
+    }
+
     return (
         <KeybordSafeview style={styles.container} >
             <View style={styles.inputContainer}>
-                <TextInput value="買い物リスト" multiline style={styles.input} />
+                <TextInput
+                    value={body}
+                    multiline
+                    style={styles.input}
+                    onChangeText={(text) => { setBody(text); }}
+                />
             </View>
             <CircleButton
                 name="check"
-                onPress={() => navigation.goBack()}
+                onPress={handlePress}
             />
         </KeybordSafeview>
     )
+}
+
+MemoEditScreen.propTypes = {
+    route: shape({
+        params: shape({ id: string, bodyText: string }),
+    }).isRequired,
 }
 
 const styles = StyleSheet.create({
